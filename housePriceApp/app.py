b34f7c1884b8b9074b06516350748cf3e70c4b78
bsaicharan18 <bsaicharan58@gmail.com>
2026-05-06 01:42:46 +0530

    Update app.py

--- housePriceApp/app.py
+++ housePriceApp/app.py
@@ -30,5 +30,8 @@ def predict():
     return render_template('index.html',
                            prediction_text=f'Predicted Price: ₹ {price_inr:,.2f}')
 # Run app
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
